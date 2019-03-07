d133c6ed30fed5e8a736f166db94891436da6ecd
zongruiqi <rqzong@kingdomai.com>
2019-03-07 11:29:11 +0800

    第一次

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="header">
+    hello
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
